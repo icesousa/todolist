5e14074b39aa6a4bc43a68a1a691cea0b8ca9735
icesousa <ice-l337elite@hotmail.com>
2022-03-24 21:17:30 -0300

    Added Text and Buttons.

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 void main() {}
 
@@ -12,9 +14,52 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.amber,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Expanded(
+          child: Container(
+            margin: EdgeInsets.all(10),
+            color: Colors.white,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Tasks',
+                      style: GoogleFonts.ubuntu(
+                          color: Color(0xff769FCE),
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.add_circle),
+                    color: Colors.blue,
+                    tooltip: 'New task',
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    DateFormat('MMMM,dd').format(DateTime.now()),
+                    style: GoogleFonts.ubuntu(
+                      fontSize: 11,
+                      color: Color(0xff95a5a6),
+                    ),
+                  ),
+                ],
+              )
+            ]),
+          ),
+        ),
       ),
     );
   }
